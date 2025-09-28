92327ee44d640db8c4826027a2eea9da29313eb6
akhil Soigama <akhilsoigama@gmail.com>
2025-09-28 13:08:55 +0530

    Add ts-node-maintained

--- ace.js
+++ ace.js
@@ -20,4 +20,8 @@
  * Register hook to process TypeScript files using ts-node
  */
 import 'ts-node-maintained/register/esm'
+
+/**
+ * Import ace console entrypoint
+ */
 await import('./bin/console.js')
--- bin/server.ts
+++ bin/server.ts
@@ -1,17 +1,33 @@
+/*
+|--------------------------------------------------------------------------
+| HTTP server entrypoint
+|--------------------------------------------------------------------------
+|
+| The "server.ts" file is the entrypoint for starting the AdonisJS HTTP
+| server. Either you can run this file directly or use the "serve"
+| command to run this file and monitor file changes
+|
+*/
+
 import 'reflect-metadata'
 import { Ignitor, prettyPrintError } from '@adonisjs/core'
 
+/**
+ * URL to the application root. AdonisJS need it to resolve
+ * paths to file and directories for scaffolding commands
+ */
 const APP_ROOT = new URL('../', import.meta.url)
+
+/**
+ * The importer is used to import files in context of the
+ * application.
+ */
 const IMPORTER = (filePath: string) => {
   if (filePath.startsWith('./') || filePath.startsWith('../')) {
     return import(new URL(filePath, APP_ROOT).href)
   }
   return import(filePath)
 }
-
-// PORT aur HOST environment variables ko set karein
-process.env.PORT = process.env.PORT || '3333'
-process.env.HOST = process.env.HOST || '0.0.0.0'
 
 new Ignitor(APP_ROOT, { importer: IMPORTER })
   .tap((app) => {
@@ -22,7 +38,7 @@ new Ignitor(APP_ROOT, { importer: IMPORTER })
     app.listenIf(app.managedByPm2, 'SIGINT', () => app.terminate())
   })
   .httpServer()
-  .start() // <- no arguments
+  .start()
   .catch((error) => {
     process.exitCode = 1
     prettyPrintError(error)
