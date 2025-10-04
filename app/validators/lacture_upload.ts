06068d684dfd9a2137a157d9142ac06f9a9be99e
akhil Soigama <akhilsoigama@gmail.com>
2025-10-04 18:54:22 +0530

    issue solve

--- app/validators/lacture_upload.ts
+++ app/validators/lacture_upload.ts
@@ -4,8 +4,6 @@ export const createLectureValidator = vine.compile(
   vine.object({
     title: vine.string().trim().minLength(2).maxLength(100),
     description: vine.string().trim().maxLength(500).optional(),
-    videoFile: vine.any(),        
-    thumbnailFile: vine.any(),    
   })
 )
 
@@ -13,7 +11,6 @@ export const updateLectureValidator = vine.compile(
   vine.object({
     title: vine.string().trim().minLength(2).maxLength(100).optional(),
     description: vine.string().trim().maxLength(500).optional(),
-    thumbnailFile: vine.any().optional(),
   })
 )
 
