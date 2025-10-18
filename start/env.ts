57c231ca729f504bbad4b8450c20018235756c09
akhil Soigama <akhilsoigama@gmail.com>
2025-10-18 08:37:48 +0530

    check database connection

--- start/env.ts
+++ start/env.ts
@@ -1,3 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Environment variables service
+|--------------------------------------------------------------------------
+|
+| The `Env.create` method creates an instance of the Env service. The
+| service validates the environment variables and also cast values
+| to JavaScript data types.
+|
+*/
+
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
@@ -16,6 +27,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
-  DB_SSL: Env.schema.boolean.optional(), 
+  DB_DATABASE: Env.schema.string()
 })
