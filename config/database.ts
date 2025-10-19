b2c81dc871bdfc1ca85f56d146cafa21acef92b4
akhil Soigama <akhilsoigama@gmail.com>
2025-10-19 12:49:14 +0530

    ips4 issue solve

--- config/database.ts
+++ config/database.ts
@@ -6,10 +6,20 @@ const dbConfig = defineConfig({
   connections: {
     postgres: {
       client: 'pg',
-      connection: {
-        connectionString: env.get('DATABASE_URL'),
-        ssl: env.get('DB_SSL', true) ? { rejectUnauthorized: false } : false,
-      },
+      connection: env.get('DATABASE_URL')
+        ? {
+            connectionString: env.get('DATABASE_URL'),
+            ssl: env.get('DB_SSL', true) ? { rejectUnauthorized: false } : false,
+          }
+        : {
+            host: env.get('DB_HOST'),
+            port: env.get('DB_PORT'),
+            user: env.get('DB_USER'),
+            password: env.get('DB_PASSWORD'),
+            database: env.get('DB_DATABASE'),
+            ssl: env.get('DB_SSL', true) ? { rejectUnauthorized: false } : false,
+          },
+
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
