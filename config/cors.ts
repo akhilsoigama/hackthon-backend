d88d145b243a9f6e930f3de3b9c6aeffe8a5881a
akhil Soigama <akhilsoigama@gmail.com>
2025-10-19 14:31:10 +0530

    cors added

--- config/cors.ts
+++ config/cors.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from '@adonisjs/cors'
+import { defineConfig } from "@adonisjs/cors";
 
-/**
- * Configuration options to tweak the CORS policy. The following
- * options are documented on the official documentation website.
- *
- * https://docs.adonisjs.com/guides/security/cors
- */
-const corsConfig = defineConfig({
+export default defineConfig({
   enabled: true,
-  origin: true,
+
+  origin: [
+    'https://hackthon-web-gilt.vercel.app', 
+    'http://localhost:5173',              
+  ],
+
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
-  exposeHeaders: [],
+  exposeHeaders: ['cache-control', 'content-language', 'content-type'],
   credentials: true,
   maxAge: 90,
 })
-
-export default corsConfig
