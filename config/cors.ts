07dcd7ce3f69b344276b95876b6235c5e131169b
akhil Soigama <akhilsoigama@gmail.com>
2025-11-26 17:12:59 +0530

    change cor

--- config/cors.ts
+++ config/cors.ts
@@ -3,10 +3,9 @@ import { defineConfig } from "@adonisjs/cors";
 export default defineConfig({
   enabled: true,
 
-  // 👇 apne frontend ka origin yahan daal
   origin: [
-    'https://hackthon-web-gilt.vercel.app', // <-- change this to your frontend URL
-    'http://localhost:5173',            // for local testing
+    'https://eduhub-learn.vercel.app', 
+    'http://localhost:5173',  
   ],
 
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
